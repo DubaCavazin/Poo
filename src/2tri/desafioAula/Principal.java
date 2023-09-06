faa4ef3ffcf90b6b73d7c91ae4b75ca03c46dd46
Eduardo Cavazin <eduardotorcav@gmail.com>
2023-09-06 19:39:03 -0300

    Correção exercíco tratamento de excessão

--- src/2tri/desafioAula/Principal.java
+++ src/2tri/desafioAula/Principal.java
@@ -6,26 +6,31 @@ public class Principal {
     
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
-        int tamanho;
-        int indice;
-        int valor;
-        
+        int tamanho, indice, valor, x;
+
         System.out.println("Informe o tamanho do vetor: ");
         tamanho = tc.nextInt();
         
-        ManipulaVetor novoVetor = new ManipulaVetor(tamanho);
+        x = 0;
+        do{
+            ManipulaVetor novoVetor = new ManipulaVetor(tamanho);
 
-        System.out.println("Informe o valor a ser adicionado: ");
-        valor = tc.nextInt();
-        System.out.println("Infome o índice que deseja adicionar o valor: ");
-        indice = tc.nextInt();
+            System.out.println("Informe o valor a ser adicionado: ");
+            valor = tc.nextInt();
+            System.out.println("Infome o índice que deseja adicionar o valor: ");
+            indice = tc.nextInt();
 
-        try{
-            novoVetor.adicionaElemento(indice, valor);
-        }catch(MinhaExcessao e){
-            System.out.println(e.getMessage());
-        }
+            try{
+                novoVetor.adicionaElemento(indice, valor);
+            }catch(MinhaExcessao e){
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("Para sair digite 1: ");
+            x = tc.nextInt();
+        }while(x != 1);
         
+        tc.close();
     }
     
 }
